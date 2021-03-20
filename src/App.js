42e75f708239327d1b7b825d14b60c6f6e8736f4
ViktoriaMik <victoriamik2015@gmail.com>
2021-03-20 11:12:52 +0200

    Lesson1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import UserComponent from'./Component/Component'
+import Header from "./Component/Header/Header";
+let userArr=[
+    {name:'Viktor',age:23},
+    {name:'Oleksandr',age:33},
+    {name:'kolya',age:44}
+];
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                {/*{userArr.map(value => (<UserComponent name={value.name} age={value.age}/>))}*/}
+                {userArr.map(value => (<UserComponent {...value}/>))}
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        );
+    }
 }
 
 export default App;
